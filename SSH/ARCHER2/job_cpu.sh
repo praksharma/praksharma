8b70f5bc9e1941237040cda8ab75c21ce917ff20
Prakhar Sharma <prakhars962@gmail.com>
2024-10-21 22:45:27 +0100

    job script cpu

--- SSH/ARCHER2/job_cpu.sh
+++ SSH/ARCHER2/job_cpu.sh
@@ -6,6 +6,12 @@
 #SBATCH --account=ta157-prakhars
 #SBATCH --partition=standard
 #SBATCH --qos=standard
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+
+# matplotlib's default path is in home which is not writable
+export MPLCONFIGDIR=/work/ta157/ta157/prakhars962/.matplotlib
 
 source /work/ta157/ta157/prakhars962/venv/bin/activate
 python DeepINN/test.py
+
